92993a75ef2ec399d2b9c5cb4b1d843c40fabef9
quananh <anhquan31299@gmail.com>
2020-04-03 17:44:03 +0700

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="app">
+        <h1>xin chao</h1>
+      </div>
+    );
+  }
+}
